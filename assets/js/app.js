fb55788ce5a92c387d0b3f4e61545875bc8e73af
Maxime Petit <contactmaximepetit@gmail.com>
2021-12-01 18:56:08 +0100

    supprEtudiant

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,18 +27,37 @@ function genererNom(){
     gsap.fromTo(nomPrenom, {opacity : 0, ease: "power2.out", translateY : "20px"}, {opacity : 1, ease: "power2.out", translateY : "0px", duration: 0.5});
     let random = Math.floor(Math.random() * (liste.length));
     let randomName = liste[random];
+
+    const selectedLi = document.querySelector(`[data-etudiant = "${random}"]`);
+    selectedLi.style.textDecoration = 'line-through';
+
+    delete selectedLi.dataset.etudiant;
     
     liste.splice(random, 1);
+
+    const allLi = document.querySelectorAll("[data-etudiant]");
+
+    allLi.forEach((li, index) => {
+        li.dataset.etudiant = index;
+    })
+
+    console.log(allLi);
+    
+    console.log(liste)
     
     console.log(random);
     console.log(randomName);
-    
+
     let nameValue = randomName["nom"];
     let prenomValue = randomName["prenom"];
     
     nom.innerText = nameValue;
     prenom.innerText = prenomValue;
+
+    
+
     if(typeof(liste[0]) === 'undefined'){
         btn.removeEventListener("click", genererNom, false);
+        btn.remove();
     }
 }
