3b18cb17c1523881fc3009ef8bbc1918399a467f
Maxime Petit <contactmaximepetit@gmail.com>
2021-12-01 16:12:35 +0100

    review

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 let liste = [];
-let btn = document.getElementById('btn');
-let nom = document.getElementById('nom');
-let prenom = document.getElementById('prenom');
-let nomPrenom = document.getElementById('nomPrenom');
-let listeEtudiant = document.getElementById('listeEtudiant');
+const btn = document.getElementById('btnTirageAuSort');
+const nom = document.getElementById('nom');
+const prenom = document.getElementById('prenom');
+const nomPrenom = document.getElementById('nomPrenom');
+const listeEtudiant = document.getElementById('liste');
 
 
 btn.addEventListener('click', genererNom);
@@ -11,13 +11,13 @@ btn.addEventListener('click', genererNom);
 
 fetch("assets/liste.json").then((data) => {
     data.json().then((data) => {
-        liste = data;
+        liste = [...data];
         console.log(liste);
 
         liste.forEach((elem, index) => {
             let li = document.createElement('li');
-            li.id = `etudiant${index}`;
-            li.textContent = elem.nom + " " + elem.prenom;
+            li.setAttribute("data-etudiant", `${index}`);
+            li.textContent = `${elem.nom} ${elem.prenom}`;
             listeEtudiant.append(li);
         })
     });
@@ -25,7 +25,7 @@ fetch("assets/liste.json").then((data) => {
 
 function genererNom(){
     gsap.fromTo(nomPrenom, {opacity : 0, ease: "power2.out", translateY : "20px"}, {opacity : 1, ease: "power2.out", translateY : "0px", duration: 0.5});
-    let random = Math.floor(Math.random() * (liste.length - 0));
+    let random = Math.floor(Math.random() * (liste.length));
     let randomName = liste[random];
     
     liste.splice(random, 1);
@@ -38,8 +38,7 @@ function genererNom(){
     
     nom.innerText = nameValue;
     prenom.innerText = prenomValue;
-    if(typeof(liste[0]) == 'undefined'){
+    if(typeof(liste[0]) === 'undefined'){
         btn.removeEventListener("click", genererNom, false);
-        btn.className="hidden";
     }
 }
