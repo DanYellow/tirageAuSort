29454d360904c602fc5a0ac80b10dd3949f49d07
Danielo Jean-Louis (DanYellow) <jeanlouis.danielo@sfr.fr>
2024-03-08 15:44:23 +0100

    Improve layout

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     base: "./",
+    define: {
+        'import.meta.env.CURRENT_YEAR': JSON.stringify(new Date().getFullYear()),
+    },
 });
