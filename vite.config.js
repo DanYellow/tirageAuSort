df149a5a944ff1d738d61405e594252532f365a4
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-03-14 22:55:44 +0100

    Add script to remove non-needed json

--- vite.config.js
+++ vite.config.js
@@ -6,49 +6,12 @@ import vituum from "vituum";
 import nunjucks from "@vituum/vite-plugin-nunjucks";
 import tailwindcss from "@vituum/vite-plugin-tailwindcss";
 
-import fs from "fs";
-import { copyFile } from 'node:fs/promises';
-
-
 const paths = await globby(["src/pages/*.json"]);
 const finalJsonPaths = paths.map((item) => {
     let categoryName = item.replace("src/pages/", "");
     categoryName = categoryName.replace(".json", "");
     return categoryName;
 });
-
-
-const addHtaccessFile = () => {
-    return {
-        name: "add-htaccess-file",
-        resolveId(source) {
-            return source === "virtual-module" ? source : null;
-        },
-        async buildEnd() {
-            // const listDistFolders = ["dist/assets", "dist/images"]
-
-            // listDistFolders.forEach(async (item) => {
-            //     await copyFile('public/data/.htaccess', `${item}/.htaccess`);
-            // })
-        },
-    };
-}
-
-function removeLocalJsonFiles() {
-    return {
-        name: "remove-local-json-files",
-        resolveId(source) {
-            return source === "virtual-module" ? source : null;
-        },
-        async buildEnd() {
-            const filesNeedToExclude = await globby(["dist/data/*.json", "!dist/data/*.dist.json"]);
-            filesNeedToExclude.forEach((file) => {
-                fs.unlinkSync(file);
-            })
-            
-        },
-    };
-}
 
 export default defineConfig({
     base: "./",
@@ -65,8 +28,6 @@ export default defineConfig({
             },
         }),
         tailwindcss(),
-        removeLocalJsonFiles(),
-        addHtaccessFile(),
     ],
     define: {
         "import.meta.env.CURRENT_YEAR": JSON.stringify(
