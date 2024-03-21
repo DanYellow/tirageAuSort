814a50c7368c9e532a79c10619469d74a9a68a80
Danielo Jean-Louis (DanYellow) <jeanlouis.danielo@sfr.fr>
2024-03-21 16:12:24 +0100

    Add contraint rules

--- src/Form/Type/EloquenceContestParticipantType.php
+++ src/Form/Type/EloquenceContestParticipantType.php
@@ -7,33 +7,36 @@ use App\Entity\Formation;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Validator\Constraints\NotBlank;
+
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-
-
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class EloquenceContestParticipantType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        // $builder->add('id', HiddenType::class);
-
         $builder->add('lastname', TextType::class, [
             'label' => '<span class="text-blue-900">Nom de famille *</span>',
             "label_html" => true,
             'mapped' => true,
             'required' => true,
             'help' => 'Champ requis',
+            'constraints' => [
+                new NotBlank(),
+            ],
         ]);
         $builder->add('firstname', TextType::class, [
             'label' => 'Prénom',
             'mapped' => true,
             'required' => true,
             'help' => 'Champ requis',
+            'constraints' => [
+                new NotBlank(),
+            ],
         ]);
         $builder->add('formation', EntityType::class, [
             'class' => Formation::class,
@@ -43,18 +46,21 @@ class EloquenceContestParticipantType extends AbstractType
         ]);
         $builder->add('is_active', CheckboxType::class, [
             'label' => 'Participe au concours ?',
-            // 'mapped' => true,
             'empty_data' => true,
-            'required' => false,
-            // 'attr' => array('checked' => 'checked', 'value' => '1')
         ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
+        $constraints = [
+            'firstname' => new NotBlank(),
+            'lastname' => new NotBlank(),
+        ];
+
         $resolver->setDefaults([
             'data_class' => EloquenceContestParticipant::class,
-            "allow_extra_fields" => true,
+            'allow_extra_fields' => true,
+            'constraints' => $constraints,
         ]);
     }
 }
