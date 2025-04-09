2dc3a5962164a9a36693f85a6a3dabb1d9311c1c
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-04-09 22:47:33 +0200

    Set in italic contender' subject

--- remove-dist-json.js
+++ remove-dist-json.js
@@ -4,6 +4,7 @@ import { globby } from "globby";
 const filesNeedToExclude = await globby([
     "dist/data/*.json",
     "!dist/data/*.dist.json",
+    "!dist/data/*.local.json",
 ]);
 
 filesNeedToExclude.forEach((file) => {
