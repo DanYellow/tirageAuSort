3683d49926186a06d0fe845d26e69062341dcfe9
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-04-10 09:35:08 +0200

    Improve footer

--- copy-htaccess.js
+++ copy-htaccess.js
@@ -1,6 +1,6 @@
 import { copyFile } from "node:fs/promises";
 
-const listDistFolders = ["dist/assets", "dist/images"];
+const listDistFolders = ["dist/assets", "dist/images", "dist/fonts"];
 
 listDistFolders.forEach(async (item) => {
     await copyFile("public/data/.htaccess", `${item}/.htaccess`);
